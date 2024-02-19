9d4e519fc3470be44d9a9ceb9f37771c1bc8d55b
Ivan Sambol <ivan.sambol1999@gmail.com>
2024-02-19 21:54:54 +0100

    App done

---
+++ cyclopedia/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CyclOPediaClassPage from "./CyclOPediaClassPage";
+import Header from "./Header";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <Header />
+    <div className="row text-white">
+      <div className="col-6">
+        <span className="h1 text-warning text-center">Class Component</span>
+        <CyclOPediaClassPage />
+      </div>
+      {/* <div className="col-6">
+        <span className="h1 text-warning text-center">Func Component</span>
+        <CyclOPediaFuncPage />
+      </div> */}
+    </div>
+  </div>
+);
