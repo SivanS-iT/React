3760daf94727d233e003fedc8cb5a8dae241db2b
Ivan Sambol <ivan.sambol1999@gmail.com>
2024-03-05 20:52:05 +0100

    Add random destination (new api)

--- travelopedia/src/index.js
+++ travelopedia/src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./layout/Header";
 import DestinationIndex from "./components/DestinationIndex";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-import { destinationApi } from "./api/destinationApi";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import RandomDestination from "./components/RandomDestination";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ApiProvider api={destinationApi}>
+    <Provider store={store}>
       <Header />
       <DestinationIndex />
-    </ApiProvider>
+      <RandomDestination />
+    </Provider>
   </React.StrictMode>
 );
